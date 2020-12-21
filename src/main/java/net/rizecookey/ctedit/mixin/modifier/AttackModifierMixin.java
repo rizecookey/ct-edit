c7d6d433d7c2e4dde4a82345bb742e22862145a1
Jonas Mittnacht <rizecookey@gmail.com>
2020-12-21 23:52:07 +0100

    Fix sprint desync

--- src/main/java/net/rizecookey/ctedit/mixin/modifier/AttackModifierMixin.java
+++ src/main/java/net/rizecookey/ctedit/mixin/modifier/AttackModifierMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -12,9 +13,21 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public abstract class AttackModifierMixin {
+    @Mixin(Player.class)
+    public static abstract class PlayerMixin extends LivingEntity {
+        protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+            super(entityType, level);
+        }
+
+        @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setSprinting(Z)V"))
+        public void fixSprintDesync(Player player, boolean bl) {
+        }
+    }
+
     @Mixin(LivingEntity.class)
     public static abstract class LivingEntityMixin extends Entity {
         @Shadow public abstract ItemStack getMainHandItem();
