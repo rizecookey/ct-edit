5332bac4e7b1848f40bfc0d33d80564635e2fb47
Jonas Mittnacht <rizecookey@gmail.com>
2020-12-25 16:35:51 +0100

    Enable crits only when not sprinting

--- src/main/java/net/rizecookey/ctedit/mixin/modifier/AttackModifierMixin.java
+++ src/main/java/net/rizecookey/ctedit/mixin/modifier/AttackModifierMixin.java
@@ -1,5 +1,8 @@
 package net.rizecookey.ctedit.mixin.modifier;
 
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -11,8 +14,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -21,6 +26,14 @@ public abstract class AttackModifierMixin {
     public static abstract class PlayerMixin extends LivingEntity {
         protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
             super(entityType, level);
+        }
+
+        @Invoker
+        public abstract void setSharedFlag(int i, boolean b);
+
+        @ModifyVariable(method = "attack", at = @At(value = "STORE"), index = 8)
+        public boolean mustSprint(boolean bl) {
+            return bl && !this.isSprinting();
         }
 
         @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setSprinting(Z)V"))
@@ -44,7 +57,7 @@ public abstract class AttackModifierMixin {
 
         @Inject(method = "blockedByShield", at = @At("TAIL"))
         public void injectCritDisabling(LivingEntity livingEntity, CallbackInfo ci) {
-            boolean isCrit = !this.onClimbable() && !this.isInWater() && !this.hasEffect(MobEffects.BLINDNESS) && !this.isPassenger() && this.fallDistance > 0.0F && !this.onGround;
+            boolean isCrit = !this.onClimbable() && !this.isInWater() && !this.hasEffect(MobEffects.BLINDNESS) && !this.isPassenger() && this.fallDistance > 0.0F && !this.onGround && !this.isSprinting();
             if (!(this.getMainHandItem().getItem() instanceof AxeItem) && this.getAttackStrengthScale(0.0F) >= 2.0F && isCrit) {
                 livingEntity.disableShield(0.8F);
             }
